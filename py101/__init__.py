1a1c817bd80606813541741fdcff0e79e4c12315
Pablo Ricco <pricco@gmail.com>
2017-05-18 22:04:11 -0700

    Bump version: 0.0.1 → 0.0.2

--- py101/__init__.py
+++ py101/__init__.py
@@ -7,7 +7,7 @@ from . import (introduction, variables, lists, operators, formatting, strings,
 
 __author__ = """Sophilabs"""
 __email__ = 'hi@sophilabs.co'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 class Story(BaseStory):
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='py101',
-    version='0.0.1',
+    version='0.0.2',
     description='Learn Python Basics from the command line.',
     long_description=readme + '\n\n' + history,
     author='Sophilabs',
